e5d16fc93f0898337dcb5df4762848af056a2a49
arko24 <62396421+arko24@users.noreply.github.com>
2022-11-04 21:33:09 +0700

    Add files via upload

--- script.js
+++ script.js
@@ -13,3 +13,13 @@ window.addEventListener("load", function() {
     })
   });
 });
+
+function checkOnlyOne(b){
+
+  var x = document.getElementsByClassName('daychecks');
+  var i;
+  
+  for (i = 0; i < x.length; i++) {
+    if(x[i].value != b) x[i].checked = false;
+  }
+  }
